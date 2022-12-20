b74301c4683fda763ab8b1a4a37872c0decf3f30
Ramonee <107966914+PedroRamonee@users.noreply.github.com>
2022-12-19 21:44:26 -0300

    Add files via upload

--- Js da dudinha.js
+++ Js da dudinha.js
@@ -5,7 +5,7 @@ img.addEventListener('click', changeImg)
 var num, sum = 'y'
 num = 1
 function changeImg() {
-    if (num < 4) {
+    if (num < 17) {
       num++    
       
         if(num == 2){
@@ -20,6 +20,15 @@ function changeImg() {
         if  (num == 4) {
             text.innerHTML = 'Pra finalizar eu queria deixar claro que voce é unica mulher que eu tenho vontade de ficar perto, de dormir junto, de beijar e de passar todos os dias da minha vida do lado, você é o amor da minha vida'
             h2.innerHTML = ' ENCERRANDO'
+        }
+        if (num == 5){
+            text.innerHTML = 'Agora vai ser uma coletania de fotos juntas nossa em que eu estou muito feliz, pq eu sou muito feliz ao seu lado'
+            h2.innerHTML = ' IMAGENS'
+
+        }
+        if (num >= 6 && num <= 17) {
+            text.innerHTML = 'Você me faz sentir o homem mais feliz e sortudo do mundo, obrigado por estar ao meu lado'
+            h2.innerHTML = ' VOCÊ É TUDO PRA MIM'
         }
 
     } else {
